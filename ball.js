e726bf34caa87bf184c0af011299bfc94cf821ef
Vaibhav Singh <61027919+vaibhav1997756@users.noreply.github.com>
2021-12-19 15:17:40 +0530

    Add files via upload

---
+++ ball.js
@@ -0,0 +1,26 @@
+let ove = document.querySelector('.ove');
+let moveBy = 10;
+
+window.addEventListener('load', () => {
+    ove.style.position = 'absolute';
+    ove.style.left = 0;
+    ove.style.top = 0;
+});
+
+
+window.addEventListener('keyup', (e) => {
+    switch (e.key) {
+        case 'ArrowLeft':
+            ove.style.left = parseInt(ove.style.left) - moveBy + 'px';
+            break;
+        case 'ArrowRight':
+            ove.style.left = parseInt(ove.style.left) + moveBy + 'px';
+            break;
+        case 'ArrowUp':
+            ove.style.top = parseInt(ove.style.top) - moveBy + 'px';
+            break;
+        case 'ArrowDown':
+            ove.style.top = parseInt(ove.style.top) + moveBy + 'px';
+            break;
+    }
+});
